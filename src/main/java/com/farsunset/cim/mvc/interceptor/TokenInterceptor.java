ea0894f681424fc3972cdbfe91c646c1ac0f131c
ichaoge <1170193113@qq.com>
2023-04-16 20:56:30 +0800

    daily update

--- src/main/java/com/farsunset/cim/mvc/interceptor/TokenInterceptor.java
+++ src/main/java/com/farsunset/cim/mvc/interceptor/TokenInterceptor.java
@@ -1,24 +1,3 @@
-/*
- * Copyright 2013-2019 Xia Jun(3979434@qq.com).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- ***************************************************************************************
- *                                                                                     *
- *                        Website : https://www.ichaoge.space                           *
- *                                                                                     *
- ***************************************************************************************
- */
 package com.farsunset.cim.mvc.interceptor;
 
 import com.farsunset.cim.annotation.AccessToken;
@@ -28,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -41,8 +19,11 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     private static final String HEADER_TOKEN = "access-token";
 
-    @Resource
-    private AccessTokenService accessTokenService;
+    private final AccessTokenService accessTokenService;
+
+    public TokenInterceptor(AccessTokenService accessTokenService) {
+        this.accessTokenService = accessTokenService;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
