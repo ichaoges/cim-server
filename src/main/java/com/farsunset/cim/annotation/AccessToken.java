367368b2ac8168f8816b91db9534d5c5834c6811
ichaoge <ichaoges@qq.com>
2023-05-09 21:55:45 +0800

    update

--- src/main/java/com/farsunset/cim/annotation/AccessToken.java
+++ src/main/java/com/farsunset/cim/annotation/AccessToken.java
@@ -1,6 +1,7 @@
 package com.farsunset.cim.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -8,8 +9,9 @@ import java.lang.annotation.Target;
 /**
  * 用户标注通过 token查到的用户账号，注入到Controller参数里面
  */
-@Target({ElementType.PARAMETER})
+@Target({ ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface AccessToken {
 
 }
