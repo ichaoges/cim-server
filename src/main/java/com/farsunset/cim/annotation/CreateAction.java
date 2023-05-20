df0511f58608169bfb87c390f1cc0af4e52ea5c6
ichaoge <ichaoges@qq.com>
2023-05-20 20:18:18 +0800

    daily update

--- src/main/java/com/farsunset/cim/annotation/CreateAction.java
+++ src/main/java/com/farsunset/cim/annotation/CreateAction.java
@@ -1,8 +1,10 @@
 package com.farsunset.cim.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CreateAction {
 
