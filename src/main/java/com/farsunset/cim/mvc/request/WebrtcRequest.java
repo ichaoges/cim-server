f8d2163d5f24a69ba088843d246ae0b407a7c25b
ichaoge <ichaoges@qq.com>
2023-05-23 22:45:55 +0800

    daily update

--- src/main/java/com/farsunset/cim/mvc/request/WebrtcRequest.java
+++ src/main/java/com/farsunset/cim/mvc/request/WebrtcRequest.java
@@ -3,12 +3,14 @@ package com.farsunset.cim.mvc.request;
 import com.farsunset.cim.annotation.CreateAction;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @ApiModel("单人通话ice、offer、answer同步请求体")
+@Data
 public class WebrtcRequest implements Serializable {
 
     @NotNull(message = "UID不能为空", groups = CreateAction.class)
@@ -18,20 +20,4 @@ public class WebrtcRequest implements Serializable {
     @NotEmpty(message = "content不能超过2000个字符", groups = CreateAction.class)
     @ApiModelProperty("ice信息json、offer或者answer的sdp")
     private String content;
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
